13acdb8904fcb9d8bcfc915f8e7b50bad33460bc
Aerindor of Arcane <jake.neuendorf@gmail.com>
2025-11-04 15:05:36 -0500

    Revert "custom armor"
    This reverts commit e2ff01207ccaff3864fac512076657cabab48e54.

--- src/main/java/net/kaupenjoe/tutorialmod/item/ModCreativeModTabs.java
+++ src/main/java/net/kaupenjoe/tutorialmod/item/ModCreativeModTabs.java
@@ -36,11 +36,6 @@ public class ModCreativeModTabs {
                         pOutput.accept(ModItems.SAPPHIRE_SHOVEL.get());
                         pOutput.accept(ModItems.SAPPHIRE_HOE.get());
 
-                        pOutput.accept(ModItems.SAPPHIRE_HELMET.get());
-                        pOutput.accept(ModItems.SAPPHIRE_CHESTPLATE.get());
-                        pOutput.accept(ModItems.SAPPHIRE_LEGGINGS.get());
-                        pOutput.accept(ModItems.SAPPHIRE_BOOTS.get());
-
                         pOutput.accept(ModBlocks.SAPPHIRE_BLOCK.get());
                         pOutput.accept(ModBlocks.RAW_SAPPHIRE_BLOCK.get());
 
