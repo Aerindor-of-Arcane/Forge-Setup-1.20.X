387d55949df6d2b50ffc83e22cdf4a2960de6d3b
Aerindor of Arcane <jake.neuendorf@gmail.com>
2025-11-04 15:05:29 -0500

    Revert "global loot modifiers"
    This reverts commit 9724ad07f24555b9eaae8db96833651bcd3170c3.

--- src/main/java/net/kaupenjoe/tutorialmod/TutorialMod.java
+++ src/main/java/net/kaupenjoe/tutorialmod/TutorialMod.java
@@ -4,7 +4,6 @@ import com.mojang.logging.LogUtils;
 import net.kaupenjoe.tutorialmod.block.ModBlocks;
 import net.kaupenjoe.tutorialmod.item.ModCreativeModTabs;
 import net.kaupenjoe.tutorialmod.item.ModItems;
-import net.kaupenjoe.tutorialmod.loot.ModLootModifiers;
 import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
@@ -31,8 +30,6 @@ public class TutorialMod {
 
         ModItems.register(modEventBus);
         ModBlocks.register(modEventBus);
-
-        ModLootModifiers.register(modEventBus);
 
         modEventBus.addListener(this::commonSetup);
 
