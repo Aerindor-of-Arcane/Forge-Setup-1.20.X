da28ad863f40164b683cd2a60c5424c51425472f
Aerindor of Arcane <jake.neuendorf@gmail.com>
2025-11-04 15:05:14 -0500

    Revert "flowers"
    This reverts commit a4d66ec1fc149fa554dd628392b712ec5b3a32f3.

--- src/main/java/net/kaupenjoe/tutorialmod/datagen/ModItemModelProvider.java
+++ src/main/java/net/kaupenjoe/tutorialmod/datagen/ModItemModelProvider.java
@@ -76,8 +76,6 @@ public class ModItemModelProvider extends ItemModelProvider {
         trimmedArmorItem(ModItems.SAPPHIRE_CHESTPLATE);
         trimmedArmorItem(ModItems.SAPPHIRE_LEGGINGS);
         trimmedArmorItem(ModItems.SAPPHIRE_BOOTS);
-
-        simpleBlockItemBlockTexture(ModBlocks.CATMINT);
     }
 
     // Shoutout to El_Redstoniano for making this
@@ -169,11 +167,5 @@ public class ModItemModelProvider extends ItemModelProvider {
         return withExistingParent(item.getId().getPath(),
                 new ResourceLocation("item/generated")).texture("layer0",
                 new ResourceLocation(TutorialMod.MOD_ID,"item/" + item.getId().getPath()));
-    }
-
-    private ItemModelBuilder simpleBlockItemBlockTexture(RegistryObject<Block> item) {
-        return withExistingParent(item.getId().getPath(),
-                new ResourceLocation("item/generated")).texture("layer0",
-                new ResourceLocation(TutorialMod.MOD_ID,"block/" + item.getId().getPath()));
     }
 }
--- src/main/java/net/kaupenjoe/tutorialmod/item/ModCreativeModTabs.java
+++ src/main/java/net/kaupenjoe/tutorialmod/item/ModCreativeModTabs.java
@@ -68,8 +68,6 @@ public class ModCreativeModTabs {
                         pOutput.accept(ModBlocks.SAPPHIRE_DOOR.get());
                         pOutput.accept(ModBlocks.SAPPHIRE_TRAPDOOR.get());
 
-                        pOutput.accept(ModBlocks.CATMINT.get());
-
                     })
                     .build());
 
