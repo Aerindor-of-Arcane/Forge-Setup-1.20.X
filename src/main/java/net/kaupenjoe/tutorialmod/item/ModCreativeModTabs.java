cacddba3ec00630532eda4a71d016dc42512fa6f
Aerindor of Arcane <jake.neuendorf@gmail.com>
2025-11-04 15:06:11 -0500

    Revert "advanced block"
    This reverts commit cf7b446e139b3af2f3e6a2d43bddced6bb32d471.

--- src/main/java/net/kaupenjoe/tutorialmod/item/ModCreativeModTabs.java
+++ src/main/java/net/kaupenjoe/tutorialmod/item/ModCreativeModTabs.java
@@ -34,8 +34,6 @@ public class ModCreativeModTabs {
                         pOutput.accept(ModBlocks.NETHER_SAPPHIRE_ORE.get());
                         pOutput.accept(ModBlocks.END_STONE_SAPPHIRE_ORE.get());
 
-                        pOutput.accept(ModBlocks.SOUND_BLOCK.get());
-
                     })
                     .build());
 
