5ed0d3bda3e09ac26dd681dadd204ab5f144f93b
Aerindor of Arcane <jake.neuendorf@gmail.com>
2025-11-04 15:03:23 -0500

    Revert "custom throwable projectile"
    This reverts commit c8c818e3a68befc85f7e13c8fa0b9ec6ced9e32e.

--- src/main/java/net/kaupenjoe/tutorialmod/datagen/ModItemModelProvider.java
+++ src/main/java/net/kaupenjoe/tutorialmod/datagen/ModItemModelProvider.java
@@ -88,8 +88,6 @@ public class ModItemModelProvider extends ItemModelProvider {
 
         simpleItem(ModItems.PINE_BOAT);
         simpleItem(ModItems.PINE_CHEST_BOAT);
-
-        simpleItem(ModItems.DICE);
     }
 
     // Shoutout to El_Redstoniano for making this
