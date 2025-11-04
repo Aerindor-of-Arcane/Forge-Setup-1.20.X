d2ae8e33b003768bf0425fca50bf29bf6b6059e6
Aerindor of Arcane <jake.neuendorf@gmail.com>
2025-11-04 13:03:04 -0500

    Revert "Squashed commit of the following:"
    This reverts commit 3eaa8d01c6cad6ce69b7218643375f597d59ae19.

--- src/main/java/net/aerindorarcane/basemod/BaseMod.java
+++ src/main/java/net/aerindorarcane/basemod/BaseMod.java
@@ -1,33 +1,6 @@
 package net.aerindorarcane.basemod;
 
 import com.mojang.logging.LogUtils;
-import net.kaupenjoe.tutorialmod.block.ModBlocks;
-import net.kaupenjoe.tutorialmod.block.entity.ModBlockEntities;
-import net.kaupenjoe.tutorialmod.entity.ModEntities;
-import net.kaupenjoe.tutorialmod.entity.client.ModBoatRenderer;
-import net.kaupenjoe.tutorialmod.entity.client.RhinoRenderer;
-import net.kaupenjoe.tutorialmod.item.ModCreativeModTabs;
-import net.kaupenjoe.tutorialmod.item.ModItems;
-import net.kaupenjoe.tutorialmod.loot.ModLootModifiers;
-import net.kaupenjoe.tutorialmod.recipe.ModRecipes;
-import net.kaupenjoe.tutorialmod.screen.GemPolishingStationScreen;
-import net.kaupenjoe.tutorialmod.screen.ModMenuTypes;
-import net.kaupenjoe.tutorialmod.sound.ModSounds;
-import net.kaupenjoe.tutorialmod.util.ModWoodTypes;
-import net.kaupenjoe.tutorialmod.villager.ModVillagers;
-import net.kaupenjoe.tutorialmod.worldgen.biome.ModTerrablender;
-import net.kaupenjoe.tutorialmod.worldgen.biome.surface.ModSurfaceRules;
-import net.kaupenjoe.tutorialmod.worldgen.tree.ModFoliagePlacers;
-import net.kaupenjoe.tutorialmod.worldgen.tree.ModTrunkPlacerTypes;
-import net.minecraft.client.gui.screens.MenuScreens;
-import net.minecraft.client.renderer.Sheets;
-import net.minecraft.client.renderer.entity.EntityRenderer;
-import net.minecraft.client.renderer.entity.EntityRenderers;
-import net.minecraft.client.renderer.entity.ThrownItemRenderer;
-import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.item.CreativeModeTabs;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.FlowerPotBlock;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -39,7 +12,6 @@ import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
-import terrablender.api.SurfaceRuleManager;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(BaseMod.MOD_ID)
@@ -50,26 +22,6 @@ public class BaseMod {
     public BaseMod() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
-        ModCreativeModTabs.register(modEventBus);
-
-        ModItems.register(modEventBus);
-        ModBlocks.register(modEventBus);
-
-        ModLootModifiers.register(modEventBus);
-        ModVillagers.register(modEventBus);
-
-        ModSounds.register(modEventBus);
-        ModEntities.register(modEventBus);
-
-        ModBlockEntities.register(modEventBus);
-        ModMenuTypes.register(modEventBus);
-
-        ModRecipes.register(modEventBus);
-        ModTrunkPlacerTypes.register(modEventBus);
-
-        ModFoliagePlacers.register(modEventBus);
-        ModTerrablender.registerBiomes();
-
         modEventBus.addListener(this::commonSetup);
 
         MinecraftForge.EVENT_BUS.register(this);
@@ -77,18 +29,12 @@ public class BaseMod {
     }
 
     private void commonSetup(final FMLCommonSetupEvent event) {
-        event.enqueueWork(() -> {
-            ((FlowerPotBlock) Blocks.FLOWER_POT).addPlant(ModBlocks.CATMINT.getId(), ModBlocks.POTTED_CATMINT);
 
-            SurfaceRuleManager.addSurfaceRules(SurfaceRuleManager.RuleCategory.OVERWORLD, MOD_ID, ModSurfaceRules.makeRules());
-        });
     }
 
+    // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
-        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
-            event.accept(ModItems.SAPPHIRE);
-            event.accept(ModItems.RAW_SAPPHIRE);
-        }
+
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
@@ -102,15 +48,7 @@ public class BaseMod {
     public static class ClientModEvents {
         @SubscribeEvent
         public static void onClientSetup(FMLClientSetupEvent event) {
-            Sheets.addWoodType(ModWoodTypes.PINE);
 
-            EntityRenderers.register(ModEntities.RHINO.get(), RhinoRenderer::new);
-            EntityRenderers.register(ModEntities.MOD_BOAT.get(), pContext -> new ModBoatRenderer(pContext, false));
-            EntityRenderers.register(ModEntities.MOD_CHEST_BOAT.get(), pContext -> new ModBoatRenderer(pContext, true));
-
-            EntityRenderers.register(ModEntities.DICE_PROJECTILE.get(), ThrownItemRenderer::new);
-
-            MenuScreens.register(ModMenuTypes.GEM_POLISHING_MENU.get(), GemPolishingStationScreen::new);
         }
     }
 }
