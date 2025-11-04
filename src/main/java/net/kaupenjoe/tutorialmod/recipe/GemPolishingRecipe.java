0b136af19b7fa59d39882c6709fe2f888c8bb4da
Aerindor of Arcane <jake.neuendorf@gmail.com>
2025-11-04 15:04:36 -0500

    Revert "jei comapt"
    This reverts commit ebe192ee05a3c32940f1e6ccc4c010cbd0fb17d7.

--- src/main/java/net/kaupenjoe/tutorialmod/recipe/GemPolishingRecipe.java
+++ src/main/java/net/kaupenjoe/tutorialmod/recipe/GemPolishingRecipe.java
@@ -35,11 +35,6 @@ public class GemPolishingRecipe implements Recipe<SimpleContainer> {
     }
 
     @Override
-    public NonNullList<Ingredient> getIngredients() {
-        return inputItems;
-    }
-
-    @Override
     public ItemStack assemble(SimpleContainer pContainer, RegistryAccess pRegistryAccess) {
         return output.copy();
     }
