1beb63bae0cd23ff80c70cf4a8c8e57803ee0d31
Aerindor of Arcane <jake.neuendorf@gmail.com>
2025-11-04 15:06:14 -0500

    Revert "advanced item"
    This reverts commit 1eb6a89566b6edaf1a4720fdbaea1f0583d86399.

--- src/main/java/net/kaupenjoe/tutorialmod/item/ModCreativeModTabs.java
+++ src/main/java/net/kaupenjoe/tutorialmod/item/ModCreativeModTabs.java
@@ -22,8 +22,6 @@ public class ModCreativeModTabs {
                         pOutput.accept(ModItems.SAPPHIRE.get());
                         pOutput.accept(ModItems.RAW_SAPPHIRE.get());
 
-                        pOutput.accept(ModItems.METAL_DETECTOR.get());
-
                         pOutput.accept(Items.DIAMOND);
 
                         pOutput.accept(ModBlocks.SAPPHIRE_BLOCK.get());
