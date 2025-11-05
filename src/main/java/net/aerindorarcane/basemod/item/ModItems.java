ecd8eb396dd9a39c14c392c8bd45c0e039a9d7c5
Aerindor of Arcane <jake.neuendorf@gmail.com>
2025-11-05 09:37:55 -0500

    Init Setup of Items

---
+++ src/main/java/net/aerindorarcane/basemod/item/ModItems.java
@@ -0,0 +1,12 @@
+package net.aerindorarcane.basemod.item;
+
+import net.aerindorarcane.basemod.BaseMod;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, BaseMod.MOD_ID);
+
+}
